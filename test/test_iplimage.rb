b1c217884e68ea441b3b7852821094c08e6858cd
ser1zw <azariahsawtikes@gmail.com>
2011-04-16 22:54:17 +0900

    moved tests of IplImage#smoothness to test_iplimage.rb

--- test/test_iplimage.rb
+++ test/test_iplimage.rb
@@ -8,6 +8,10 @@ include OpenCV
 
 # Tests for OpenCV::IplImage
 class TestIplImage < OpenCVTestCase
+  def should_classify_images_as(filename, classification)
+    assert_equal(OpenCV::IplImage::load(filename, OpenCV::CV_LOAD_IMAGE_GRAYSCALE).smoothness[0], classification)
+  end
+
   def test_initialize
     img = IplImage.new(10, 20)
     assert_equal(10, img.width)
@@ -126,6 +130,26 @@ class TestIplImage < OpenCVTestCase
     # Alias
     img.coi = 1
     assert_equal(1, img.coi)
+  end
+
+  def test_smoothness
+    asset_path = File.join(File.dirname(__FILE__), 'samples')
+
+    for image in Array.new(7) { |e| e = File.join(asset_path, "smooth%d.jpg") % e } do
+      should_classify_images_as image, :smooth
+    end
+
+    for image in Array.new(2) { |e| e = File.join(asset_path, "messy%d.jpg") % e } do
+      should_classify_images_as image, :messy
+    end
+
+    for image in Array.new(10) { |e| e = File.join(asset_path, "blank%d.jpg") % e } do
+      should_classify_images_as image, :blank
+    end
+
+    for image in Array.new(2) { |e| e = File.join(asset_path, "partially_blank%d.jpg") % e } do
+      should_classify_images_as image, :blank
+    end
   end
 end
 
